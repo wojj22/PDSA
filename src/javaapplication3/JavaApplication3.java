d633f754c945e77c4e07dc4490ed77dfcd9f0bf8
Lahiru531 <127671975+Lahiru531@users.noreply.github.com>
2024-02-20 23:35:54 +0530

    Update JavaApplication3.java

--- src/javaapplication3/JavaApplication3.java
+++ src/javaapplication3/JavaApplication3.java
@@ -1,9 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package javaapplication3;
 
+package javaapplication3;
 
 
 
